bdcf03c77328897404f21829f8886dd840df00b3
Diana <anaidlizana8@gmail.com>
2023-04-06 12:51:08 -0500

    prueba

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("hola mundo");
+let varibleLet={
+'nombre' :'Diana',
+'apellido':'lizana',
+};
+
+var a =12;
+const variableConst=123;
+
+console.log(a);
+console.log(variableConst);
+console.log(varibleLet.nombre + " " +  varibleLet.apellido);
+
+
+
